dabe4a1b15212bf9f0de38c3af53af1aa6dbe4db
Улукбек Эселов <ulukbekeselov@gmail.com>
2021-09-21 14:18:30 +0600

    initial commit

---
+++ src/main/java/kg/megacom/shopkg/ShopKgApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.shopkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopKgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megacom/shopkg/ShopKgApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.shopkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
